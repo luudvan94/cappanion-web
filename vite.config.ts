147442cd34642be6f14e8811419294e8fb62e254
Luu Van <luudvan94@gmail.com>
2024-11-02 18:43:02 -0400

    add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/cappanion/',
   test: {
     globals: true,
     environment: 'happy-dom',
