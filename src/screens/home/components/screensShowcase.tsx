4a797538ed6b0f7244cc66ee6cee94de8f601593
Luu Van <luudvan94@gmail.com>
2024-11-03 10:23:49 -0500

    feat: integrate waitlist

--- src/screens/home/components/screensShowcase.tsx
+++ src/screens/home/components/screensShowcase.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable tailwindcss/no-custom-classname */
+import { useEffect, useRef, useState } from 'react'
+import TypeIt from 'typeit'
+
 export type ScreenShowcase = {
   id: string
   title: string
@@ -9,49 +12,47 @@ export type ScreenShowcase = {
 export const screensShowcase: ScreenShowcase[] = [
   {
     id: '1',
-    title: 'Tracking',
-    desc: 'Track your emotions, activities, and more.',
+    title: 'Track your memories',
+    desc: 'Be it a note, a thought, or anything else. Cappanion will help you remember it. We believe being able to see the progress of your memories is key to understanding yourself.',
     imgFileName: 'homescreen.png'
   },
   {
     id: '2',
-    title: 'Add',
-    desc: 'Add a tweet, note, or anything else to your memory.',
+    title: 'Easy to add',
+    desc: 'We designed Cappanion to be easy as possible to add new memories.',
     imgFileName: 'add_tweet.png'
   },
   {
     id: '3',
-    title: 'Search',
-    desc: 'Search your memories by keyword.',
+    title: 'Simple search',
+    desc: 'Lightning fast search for your memories. Any keyword will do. Cappanion will show you all the memories that match your query.',
     imgFileName: 'search_tweet.png'
   },
   {
     id: '4',
-    title: 'Ask',
-    desc: 'Ask Cappanion questions about your memories.',
+    title: 'Ambiguous questions',
+    desc: 'Ask Cappanion questions about your memories. We will do our best to understand you.',
     imgFileName: 'memory_search.png'
   },
   {
     id: '5',
-    title: 'Summarize',
-    desc: 'Summarize your memories.',
+    title: 'Summarize your life',
+    desc: 'Cappanion will summarize your memories for you. This is a great way to see the progress of your life.',
     imgFileName: 'summarize_life.png'
   },
   {
     id: '6',
     title: 'Emotional Support',
-    desc: 'Get emotional support from Cappanion.',
+    desc: 'We build a profile that match your characteristics. Not only your personalities, but also goals, concerns, and more.',
     imgFileName: 'emotional_support.png'
   },
   {
     id: '7',
     title: 'Dark Mode',
-    desc: 'Cappanion is available in dark mode.',
+    desc: 'Of course, Cappanion is available in dark mode.',
     imgFileName: 'dark_mode.png'
   }
 ]
-
-import { useEffect, useRef, useState } from 'react'
 
 export const ScreensShowcase = () => {
   const overlayImage = useRef<ScreenShowcase | null>(null)
@@ -59,6 +60,7 @@ export const ScreensShowcase = () => {
   const mousePosition = useRef({ x: 0, y: 0 })
   const [activeScreenId, setActiveScreenId] = useState<string | null>(null)
   const [isSingleColumn, setIsSingleColumn] = useState(false)
+  const activeScreenRef = useRef<HTMLParagraphElement | null>(null)
 
   useEffect(() => {
     const handleResize = () => {
@@ -70,6 +72,21 @@ export const ScreensShowcase = () => {
 
     return () => window.removeEventListener('resize', handleResize)
   }, [])
+
+  useEffect(() => {
+    new TypeIt('#description', {
+      speed: 50
+    }).go()
+  }, [])
+
+  useEffect(() => {
+    if (activeScreenRef.current && isSingleColumn) {
+      activeScreenRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+      })
+    }
+  }, [activeScreenId, isSingleColumn])
 
   const handleMouseMove = (event: React.MouseEvent) => {
     if (isSingleColumn) return // Disable hover logic for single column
@@ -93,12 +110,23 @@ export const ScreensShowcase = () => {
         style={{ scrollPaddingTop: '1rem', scrollPaddingBottom: '1rem' }}
       >
         <div>
-          <h1 className="prose text-center text-2xl font-bold text-white">
+          <h1 className="prose text-center font-mono text-4xl font-bold text-white">
             Cappanion
           </h1>
-          <p className="mb-4 text-center text-lg text-gray-300">
+          <p
+            id="description"
+            className="mb-4 text-center font-mono text-lg text-gray-300"
+          >
             Your personal AI companion
           </p>
+          <a
+            href="https://getwaitlist.com/waitlist/21845"
+            className="block text-center font-mono text-lg text-blue-500 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Join our waitlist
+          </a>
         </div>
         <div className="no-scrollbar grid grow grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 md:gap-12 lg:gap-36">
           {screensShowcase.map((screen) => (
@@ -114,7 +142,7 @@ export const ScreensShowcase = () => {
                 }}
                 src={`/${screen.imgFileName}`}
                 alt={screen.title}
-                className="scale-120 h-auto w-full"
+                className="h-auto w-full"
               />
               {!isSingleColumn && (
                 <div
@@ -141,9 +169,14 @@ export const ScreensShowcase = () => {
                   }}
                 ></div>
               )}
-              <p className="mt-6 text-white">{screen.title}</p>
+              <p className="mt-6 font-mono text-white">{screen.title}</p>
               {activeScreenId === screen.id && isSingleColumn && (
-                <p className="mt-2 text-white">{screen.desc}</p>
+                <p
+                  ref={activeScreenRef}
+                  className="mt-2 font-mono text-white animate-in fade-in-75"
+                >
+                  {screen.desc}
+                </p>
               )}
             </div>
           ))}
@@ -158,14 +191,22 @@ export const ScreensShowcase = () => {
             <img
               src={`/${overlayImage.current?.imgFileName}`}
               alt={overlayImage.current?.title}
-              className="max-h-48 w-auto scale-150 md:max-h-96"
+              className="h-auto w-full scale-150 md:max-h-96"
             />
           </div>
           <div className="text-left text-white">
-            <h2 className="text-xl font-bold md:text-2xl">
+            <h2 className="font-mono text-xl font-bold md:text-2xl">
               {overlayImage.current?.title}
             </h2>
-            <p className="mt-2 text-base md:text-lg">
+            <p
+              id="overlay-description"
+              className="mt-2 font-mono text-base md:text-lg"
+              style={{
+                maxWidth: '500px', // Set a fixed max width
+                whiteSpace: 'normal', // Allow text to wrap
+                overflowWrap: 'break-word' // Break long words
+              }}
+            >
               {overlayImage.current?.desc}
             </p>
           </div>
